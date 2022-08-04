196b19560f88ca677419e05a87c09a5155f47b24
shafin_shant0 <shafinshanto9@gmail.com>
2022-08-04 23:41:35 +0600

    fixed issue

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
-const express = require('express')
-const dbConnect = require('./dbConnect')
-const itemsRoute = require('./routes/itemsRoute') 
-const userRoute = require('./routes/userRoute')
-const billsRoute = require('./routes/billsRoute') 
+const express = require("express");
+const dbConnect = require("./dbConnect");
+
+const app = express();
+app.use(express.json());
+const itemsRoute = require("./routes/itemsRoute");
+const usersRoute = require("./routes/userRoute");
+const billsRoute = require('./routes/billsRoute')
+app.use("/api/items/", itemsRoute);
+app.use("/api/users/", usersRoute);
+app.use("/api/bills/", billsRoute);
 const path = require('path')
-require("dotenv").config();
-
-
-const app = express()
-app.use(express.json())
-
-
-const port = process.env.PORT || 5000
-
-
-app.use('/api/items/', itemsRoute)
-app.use('/api/user/', userRoute)
-app.use('/api/bills', billsRoute)
 
 if(process.env.NODE_ENV==='production')
 {
@@ -26,10 +19,7 @@ if(process.env.NODE_ENV==='production')
     }) 
 }
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get("/", (req, res) => res.send("Hello World! from home api"));
+app.listen(port, () => console.log(`Node JS Server Running at port ${port}`));
